be544c926efd5183a480d17ac53688f0005ae2e1
João Vitor Domene Maganini <102229903+JoaoVDM@users.noreply.github.com>
2023-03-06 15:24:31 -0300

    Add files via upload

---
+++ JavaScript/JavaScript tipos, variáveis e funções/type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "João"
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+// teamplate string OU teamplate literal
